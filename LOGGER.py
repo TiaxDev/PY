f10ac3d9db1a11aabb55d574fd552c349ce70218
Mattia <tiawanted@hotmail.it>
2016-04-28 17:39:20 +0200

    cleaned up code +improved code quality
    - Removed easygui (password form)

--- LOGGER.py
+++ LOGGER.py
@@ -1,128 +1,88 @@
+#!/usr/bin/env python
+
+#Mattia Tanzini, 2013
+
 import mechanize
 from bs4 import *
+import getpass
 import re
-from easygui import passwordbox
-br=mechanize.Browser()
+import sys
+
+br = mechanize.Browser()
 br.set_handle_robots(False)
-def menu():
-    print("============================================================")
-    print("BENVENUTO NEL PROGRAMMA DI GESTIONE DEL REGISTRO ELETTRONICO\n(ITIS G.GALILEI-CREMA):")
-    print("============================================================\n")
-    scelta=raw_input("Che sezione vuoi visionare? (Voti, Assenze, Argomenti[BETA], Agenda): ")
-    if scelta=="voti" or scelta=="Voti" or scelta=="VOTI":
-        login()
-        getvoti()
-        menu2()
-    elif scelta=="assenze" or scelta=="Assenze" or scelta=="ASSENZA":
-        login()
-        getass()
-        menu2()
-    elif scelta=="argomenti" or scelta=="Argomenti" or scelta=="ARGOMENTI":
-        login()
-        getargo()
-        menu2()
-    elif scelta=="agenda" or scelta=="Agenda" or scelta=="AGENDA":
-        login()
-        getagenda()
-        menu2()
+
+def menu(isLoggedIn):
+
+    if(isLoggedIn == False):
+        login();
+    isLoggedIn = True
+    
+    scelta = raw_input("Che sezione vuoi visionare? (1)Voti, (2)Assenze, (3)Argomenti[BETA], (4)Agenda, (5)Uscita: ")
+
+    if scelta == "1":
+        get("https://galilei-cr-sito.registroelettronico.com/votes/", "votes")
+        return menu(isLoggedIn)
+    elif scelta == "2":
+        get("https://galilei-cr-sito.registroelettronico.com/absences/", "absences")
+        return menu(isLoggedIn)
+    elif scelta == "3":
+        get("https://galilei-cr-sito.registroelettronico.com/topics/", "topics")
+        return menu(isLoggedIn)
+    elif scelta == "4":
+        get("https://galilei-cr-sito.registroelettronico.com/agenda/", "agenda")
+        return menu(isLoggedIn)
+    elif scelta == "5":
+        print("Exit...")
+        sys.exit(0)
     else:
         print("Errore. La categoria '%s' non esiste!\n" %(scelta))
-        return menu()
+        return menu(isLoggedIn)
+    
 def login():
-        nome=raw_input("INSERISCI IL NOME UTENTE: ")
-        passw=passwordbox("INSERISCI LA PASSWORD: ",title="PASSWORD")
-        r=br.open("https://galilei-cr-sito.registroelettronico.com/votes")
+        nome = raw_input("INSERISCI IL NOME UTENTE: ")
+        passw = raw_input("INSERISCI PASSWORD (watch your back!): ");
+        print "Connessione..."
+        r = br.open("https://galilei-cr-sito.registroelettronico.com/votes")
+        print "Autenticazione..."
         br.select_form(nr=0)
-        br.form["username"]=nome
-        br.form["password"]=passw
-        r=br.submit()
-        insuccess=re.search("Nome utente e/o password errati.",r.read())
+        br.form["username"] = nome
+        br.form["password"] = passw
+        r = br.submit()
+        insuccess = re.search("Nome utente e/o password errati.", r.read())
         if insuccess is None:
             br.open("https://galilei-cr-sito.registroelettronico.com/votes")
-            scrivo=open("MainHTML.html","w")
+            scrivo = open("mainTemp.html", "w")
             scrivo.write(br.response().read())
             scrivo.close()
-            scrivo=open("MainHTML.html","r")
-            html_doc=(scrivo)
-            soup=BeautifulSoup(html_doc)
+            scrivo = open("mainTemp.html", "r")
+            html_doc = (scrivo)
+            soup = BeautifulSoup(html_doc)
             for node in soup.findAll('h2',{'id':'student_name'}):
-                nome_studente=''.join(node.findAll(text=True))
+                nome_studente = ''.join(node.findAll(text=True))
                 print("\nBenvenuto, %s!\n") %(nome_studente)
         else:
             print("\nErrore. Nome utente e/o password errati.\n")
             return login()
-def menu2():
-    while True:
-        scelta=raw_input("Che sezione vuoi visionare? (Voti, Assenze, Argomenti[BETA], Agenda): ")
-        if scelta=="voti" or scelta=="Voti" or scelta=="VOTI":
-            getvoti()
-        elif scelta=="assenze" or scelta=="Assenze" or scelta=="ASSENZA":
-            getass()
-        elif scelta=="argomenti" or scelta=="Argomenti" or scelta=="ARGOMENTI":
-            getargo()
-        elif scelta=="agenda" or scelta=="Agenda" or scelta=="AGENDA":
-            getagenda()
-        else:
-            print("Errore. La categoria '%s' non esiste!\n" %(scelta))
-            return menu2()
-def getvoti():
-    br.open("https://galilei-cr-sito.registroelettronico.com/votes")
-    scrivo=open("VotiHTML.html","w")
+        
+def get(URL, fileName):
+    br.open(URL)
+    scrivo = open("temp" + fileName + ".html", "w")
     scrivo.write(br.response().read())
     scrivo.close()
-    scrivo=open("VotiHTML.html","r")
-    html_doc=(scrivo)
-    soup=BeautifulSoup(html_doc)
-    print("\nLISTA VOTI AGGIORNATA:")
-    print("(Ordine per data)\n\n")
+    scrivo = open("temp" + fileName + ".html", "r")
+    html_doc = (scrivo)
+    soup = BeautifulSoup(html_doc)
+    print "\n(Lista aggiornata)\n"
     print("===================================================")
     for node in soup.findAll('p',{'class':'day_description'}):
         print ''.join(node.findAll(text=True))
-    print("====================================================")
+    print("====================================================\n")
     scrivo.close()
-def getass():
-    br.open("https://galilei-cr-sito.registroelettronico.com/absences")
-    scrivo=open("AssenzeHTML.html","w")
-    scrivo.write(br.response().read())
-    scrivo.close()
-    scrivo=open("AssenzeHTML.html","r")
-    html_doc=(scrivo)
-    soup=BeautifulSoup(html_doc)
-    print("\nLISTA ASSENZE AGGIORNATA:")
-    print("(Ordine per data)\n\n")
-    print("====================================================")
-    for node in soup.findAll('p',{'class':'day_description'}):
-        print ''.join(node.findAll(text=True))
-    print("====================================================")
-    scrivo.close()
-def getargo():
-    br.open("https://galilei-cr-sito.registroelettronico.com/topics")
-    scrivo=open("ArgomentiHTML.html","w")
-    scrivo.write(br.response().read())
-    scrivo.close()
-    scrivo=open("ArgomentiHTML.html","r")
-    html_doc=(scrivo)
-    soup=BeautifulSoup(html_doc)
-    print("\nLISTA ARGOMENTI AGGIORNATA:")
-    print("(Ordine per data)\n\n")
-    print("====================================================")
-    for node in soup.findAll('p',{'class':'day_description'}):
-        print ''.join(node.findAll(text=True))
-    print("====================================================")
-    scrivo.close()
-def getagenda():
-    br.open("https://galilei-cr-sito.registroelettronico.com/agenda")
-    scrivo=open("AgendaHTML.html","w")
-    scrivo.write(br.response().read())
-    scrivo.close()
-    scrivo=open("AgendaHTML.html","r")
-    html_doc=(scrivo)
-    soup=BeautifulSoup(html_doc)
-    print("\nAGENDA AGGIORNATA:")
-    print("(Ordine per data)\n\n")
-    print("====================================================")
-    for node in soup.findAll('p',{'class':'day_description'}):
-        print ''.join(node.findAll(text=True))
-    print("====================================================")
-    scrivo.close()
-menu()
+
+#Main
+
+print("===============================================")
+print("PROGRAMMA DI GESTIONE DEL REGISTRO ELETTRONICO\n(ITIS G.GALILEI-CREMA):")
+print("Author: Mattia Tanzini \nInfo: @TiaxDev (https://github.com/TiaxDev)")
+print("===============================================\n") 
+menu(False)
